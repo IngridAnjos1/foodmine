259ae759dec3471c8cadf60d39170f372c239d63
Ingrid Anjos <ingrid_anjos@hotmail.com>
2024-03-25 15:35:30 -0300

    feat: inclusao de construtor no component home

--- frontend/src/app/components/pages/home/home.component.ts
+++ frontend/src/app/components/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { FoodService } from './../../../services/food.service';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Food } from 'src/app/shared/models/food';
 
 @Component({
@@ -8,10 +9,15 @@ import { Food } from 'src/app/shared/models/food';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
- 
-  foods:Food[] = [];
-  constructor(private foodService:FoodService){
-    this.foods = foodService.getAll();
+
+  foods: Food[] = [];
+  constructor(private foodService: FoodService, activateRoute: ActivatedRoute) {
+    activateRoute.params.subscribe((params) => {
+      if (params.searchTerm)
+        this.foods = this.foodService.getAllFoodBySearchTerm(params.searchTerm);
+      else
+        this.foods = foodService.getAll();
+    })
   }
 
   ngOnInit(): void {
